cd33515719204318859e0c7a19c4780388e3203b
hsbx520 <79846141+hsbx520@users.noreply.github.com>
2025-11-17 18:43:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -464,7 +464,7 @@ const PRESALE_CONFIG = {
         USDT: "Ethereum or BNB Smart Chain.",
         USDC: "Ethereum or BNB Smart Chain."
     },
-    COUNTDOWN_TARGET_UTC: "2025-11-17T04:00:00Z"
+    COUNTDOWN_TARGET_UTC: "2025-11-18T04:00:00Z"
 };
 
 // Presale State
